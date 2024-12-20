134b104a31f6d8e209aaafb14660a4e149c8a291
Joep Atol <jatol@ilionx.com>
2024-12-20 22:26:59 +0100

    also start to add rust tests

--- aras_core/src/lib.rs
+++ aras_core/src/lib.rs
@@ -11,18 +11,18 @@ mod middleware_services;
 pub use crate::asgispec::{ASGICallable, ASGIMessage, ASGIScope, ReceiveFn, Scope, SendFn, State};
 pub use crate::error::{Error, Result};
 pub use crate::http::{
-    HTTPDisconnectEvent, HTTPRequestEvent, HTTPResonseBodyEvent, HTTPResponseStartEvent, HTTPScope,
+    HTTPDisconnectEvent, HTTPRequestEvent, HTTPResonseBodyEvent, HTTPResponseStartEvent, HTTPScope, serve_http,
 };
 pub use crate::lifespan::{
     LifespanScope, LifespanShutdown, LifespanShutdownComplete, LifespanShutdownFailed, LifespanStartup,
-    LifespanStartupComplete, LifespanStartupFailed,
+    LifespanStartupComplete, LifespanStartupFailed, LifespanHandler,
 };
 pub use crate::websocket::{
     WebsocketAcceptEvent, WebsocketCloseEvent, WebsocketConnectEvent, WebsocketDisconnectEvent, WebsocketReceiveEvent,
-    WebsocketScope, WebsocketSendEvent,
+    WebsocketScope, WebsocketSendEvent, serve_websocket,
 };
-use crate::server::Server;
-pub use crate::server::ServerConfig;
+pub use crate::application::{Application, ApplicationFactory};
+pub use crate::server::{Server, ServerConfig};
 
 pub async fn serve<S: State + 'static, T: ASGICallable<S> + 'static>(app: T, state: S, config: Option<ServerConfig>) -> Result<()> {
     let mut server = Server::new(app, state);
